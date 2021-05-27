b8c57bf06faaf3e749c63efcb5188817fcd1532d
Vitor Mendes <71814309+vitorsmends@users.noreply.github.com>
2021-05-27 14:10:28 -0300

    Update controllers.ino

--- controllers/controllers.ino
+++ controllers/controllers.ino
@@ -54,7 +54,7 @@ void desloca_direita(char* msgf){
     printf("%d: %c\n", i, msgf[i]);
   }*/
 }
-
+//lembrar de alterar os valores do potenciometro para que o limite seja 1023
 // Esta funcao cria um vetor de caracteres de 8 numeros (na forma de char e nao como int)
 // Formado respectivamente por: leitura do potenciometro 1 e 2 (0-255), botao de pausa e botao de reset (0-1)
 // a terminacao 'f' no nome de algumas variaveis foi usado para as diferenciarr das variaveis globais
